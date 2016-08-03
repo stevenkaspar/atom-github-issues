f0eb7aea152b1caebfa7bc81d96c66055015100d
stevenkaspar <srkaspar@gmail.com>
2016-08-03 17:04:28 -0500

    initial commit

---
+++ spec/atom-github-issues-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomGithubIssuesView from '../lib/atom-github-issues-view';
+
+describe('AtomGithubIssuesView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
